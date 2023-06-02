51b80547b4c477aabcb9c851dbc1fd4caa1d8dfd
maor2871 <ClassifiedAnonymouse@gmail.com>
2023-06-02 23:23:37 +0300

    Update file tree.

--- RushHour.bat
+++
@@ -1,5 +0,0 @@
-xcopy /y "%~dp0""dosboxconftemplate.txt" "%~dp0""dosbox.conf"
-ECHO mount %CD:~0,1% "%~dp0">>"%~dp0\dosbox.conf"
-ECHO %CD:~0,2%>>"%~dp0\dosbox.conf"
-ECHO RushHour>>"%~dp0\dosbox.conf"
-dosbox.exe
